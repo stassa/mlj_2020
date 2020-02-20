0d137c4cc970431d60828959fd87b07b350e287e
YeGoblynQueenne@splinter <e.patsantzis17@imperial.ac.uk>
2020-02-20 19:41:30 +0000

    Removed Thelma from the project :(

--- experiments/learning_rate/scripts/run_learning_rate.pl
+++ experiments/learning_rate/scripts/run_learning_rate.pl
@@ -5,46 +5,67 @@
 
 /** <module> Running script for learning_rate.pl experiment.
 
-Formalises running of learning rate experiment comparing Louise to
-Thelma or Metagol. Sets configuration options and other necessary
-parameters and should also act as documentation of those options for
-future reference and to repeat the experiments.
+Predicates in this module formalise running of learning curve
+experiments comparing Louise to Metagol detailed in the paper.
+
+Directives called when this module file is loaded set configuration
+options and other necessary parameters and should also act as
+documentation of those options for future reference and to repeat the
+experiments.
+
+Note that the experiment run by learning_rate.pl is named as a "learning
+curve" experiment in the paper. This module was misnamed originally and
+now it's too much work to change its name and every reference to its
+name everywhere.
 */
 
 % move/2 programs learned by Louise with a high sampling rate can be
 % quite large (over 2k clauses for a 4x4 grid) and that blows the
 % tabling space during evaluation (we table the target predicate of a
 % problem to avoid going infinite with left-recursion during
-% evaluation). 8GB seems to be enough of an increase to avoid this.
+% evaluation). 8GB seems to be enough of an increase from the default
+% Swi-Prolog table space to allow evaluation to complete without errors.
 :-set_prolog_flag(table_space, 8_589_934_592).
 
-% Must be run in the top directory of thelma or louise.
-% Running in thelma/ will run the experiments with thelma.
-% Running in louise/ will run the experiment with Aleph.
-% Nah, just kidding. Louise.
+% This script must be run with the current working directory set to the
+% top directory of Metagol or Louise. The working directory
+% determines which learner will run the experiment. Running this script
+% in <project_root>/metagol/ will run the experiments with Metagol.
+% Running it in <project_root>/louise/ will run the experiment with
+% Aleph.
+%
+% Just checking you're paying attention. Louise, not Aleph.
 :-[load_headless].
 
-% Search paths relative to the ones set in load_headless.
+% Search paths relative to the ones set in load_headless.pl in
+% <project_root>/metagol/ or <project_root>/louise/
 user:file_search_path(experiment_data, project_root('../data')).
 user:file_search_path(experiments, project_root('../experiments')).
 user:file_search_path(learning_rate, experiments(learning_rate)).
 
+% Experiment code and necessary libraries.
 :-use_module(learning_rate).
 :-use_module(learning_rate_configuration).
 :-use_module(lib(mathemancy/mathemancy)).
-% Allow dynamic setting of normally static options in Thelma.
-:-dynamic configuration:experiment_file/2
-         ,configuration:depth_limits/2.
+% Used to generate primitive moves for grid world navigation dataset
 :-use_module(experiment_data(robots/move_generator)).
 
-% Make Swi debug messages readable in default-coloured powershell.
+% Allow dynamic setting of normally static options in Louise.
+:-dynamic configuration:experiment_file/2.
+
+% Colorise Swi debug messages to make them more readable in
+% dark-coloured terminals (default colouring is too dark).
 user:message_property(debug(learning_rate), color( [ fg(cyan) ]) ).
 user:message_property(debug(learning_rate_setup), color( [ fg(cyan) ]) ).
 user:message_property(debug(run_learning_rate_setup), color( [ fg(cyan) ]) ).
 user:message_property(debug(progress), color( [ fg(yellow) ]) ).
 
+% Enables logging of experiment progress to console.
+% Turning this off improves running times but it makes it harder to know
+% when an experiment has finished running.
 :-debug(run_learning_rate_setup).
 
+% Avoids some of the warnings when consulting metagol.pl
 :-dynamic metagol:max_clauses/1
          ,metagol:max_inv_preds/1
          ,metagol:min_clauses/1
@@ -55,13 +76,29 @@ user:message_property(debug(progress), color( [ fg(yellow) ]) ).
 %
 %       Dataset configuration options.
 %
+%       These are configuration options normally set manually in
+%       configuration.pl in the root directories of Metagol and Louise
+%       or learning_rate_configuration.pl (the configuration file for
+%       the script that actually runs the experiment). These options are
+%       documented in their respective configuration files.
+%
+%       The point of declaring these options here is to keep all the
+%       configuration options needed to run the experiments in one
+%       place, so that it's easier to reproduce the experiment without
+%       having to fiddle with different configuration options.
+%
+%       If a user wishes to change the configuration (to test different
+%       experimental setups) it is recommended to do it by changing
+%       these configuration options, rather than the configuration files
+%       where these are defined (which would take some looking up
+%       anyway).
+%
 /* kin.pl config options */
 config(kin,experiment_file,['../data/kinship/kin.pl',kin]).
 config(kin,copy_plotting_scripts,[learning_rate(plotting)]).
 config(kin,logging_directory,'../experiments/learning_rate/output/kin/').
 config(kin,plotting_directory,'../experiments/learning_rate/output/kin/').
 config(kin,learning_rate_time_limit,[300]).
-config(kin,depth_limits,[40,0]).
 config(kin,reduction,[plotkins]).
 config(kin,resolutions,[5000]).
 config(kin,recursive_reduction,[true]).
@@ -89,7 +126,6 @@ config(robots,copy_plotting_scripts,[learning_rate(plotting)]).
 config(robots,logging_directory,'../experiments/learning_rate/output/robots/').
 config(robots,plotting_directory,'../experiments/learning_rate/output/robots/').
 config(robots,learning_rate_time_limit,[300]).
-config(robots,depth_limits,[40,0]).
 config(robots,reduction,[plotkins]).
 config(robots,resolutions,[5000]).
 config(robots,recursive_reduction,[true]).
@@ -106,14 +142,46 @@ config(robots,world_dimensions,[4,4]).
 %
 %       Set confiuration options for a Dataset.
 %
+%       This predicate first reads configuration options defined in
+%       config/3 clauses in this file, then sets them dynamically using
+%       the predicates set_configuration_option/2 and
+%       set_local_configuration_option/3.
+%
+%       set_configuration_option/2 is used to set generic configuration
+%       options that can also be set manually in the configuration.pl
+%       file under the root directory of each of the two learners.
+%
+%       set_local_configuration_option/3 is used to set configuration
+%       options for a) learning_rate.pl (the experiment code module) in
+%       particular, i.e. the options in learning_rate_configuration.pl,
+%       and b) experiment files' configuration modules, in particular
+%       the configuration for the move generator in the grid world
+%       navigation experiment generator_configuration.pl. A separate
+%       predicate is necessary for the experiment code because
+%       experiment code and dataset configuration options are not
+%       accessible to set_configuration_option/2. The reason for this
+%       inaccessibility is that set_configuration_option/2 calls
+%       configuration option predicates directly in the configuration
+%       module, with a prefix of "configuration:" (note the colon),
+%       whereas experiment code and experiment file configuration
+%       options are declared in their own configuration modules that are
+%       not imported into the main configuration module (to reduce the
+%       configuration module's number of options).
+%
+%       The reason for all this indirection is to allow experiments to
+%       be run without having to touch the code of the two learners.
+%       This ensures that all the configuration options that need to be
+%       set to run the experiments can be managed from a single place,
+%       this module.
+%
 setup(D):-
+        % Read configuration options (defined in this file)
         configuration:learner(L)
         ,config(D,experiment_file,Es)
         ,config(D,copy_plotting_scripts,CS)
         ,config(D,logging_directory,LD)
         ,config(D,plotting_directory,PD)
         ,config(D,learning_rate_time_limit,TL)
-        ,config(D,depth_limits,DL)
         ,config(D,reduction,R)
         ,config(D,resolutions,S)
         ,config(D,recursive_reduction,RR)
@@ -121,13 +189,14 @@ setup(D):-
         ,config(D,max_clauses,Max_C)
         ,config(D,max_inv_preds,MI)
         ,config(D,min_clauses,Min_C)
-        % Move generator options
+        % Generator options for robots dataset.
         ,(   D = robots
          ->  config(D,experiment_world,EW)
             ,config(D,world_dimensions,WD)
          ;   true
          )
-        ,set_configuration_option(experiment_file,Es)
+        % Set configuration options dynamically to the ones defined in this file.
+        ,auxiliaries:set_configuration_option(experiment_file,Es)
         ,set_local_configuration_option(learning_rate,copy_plotting_scripts,CS)
         ,set_local_configuration_option(learning_rate,logging_directory,LD)
         ,set_local_configuration_option(learning_rate,plotting_directory,PD)
@@ -135,11 +204,10 @@ setup(D):-
         ,(   D = robots
          ->  set_local_configuration_option(robots,experiment_world,EW)
             ,set_local_configuration_option(robots,world_dimensions,WD)
+            ,move_generator:write_dataset
          ;   true
          )
-        ,(   L = thelma
-         ->  set_configuration_option(depth_limits,DL)
-         ;   L = louise
+        ,(   L = louise
          ->  set_configuration_option(reduction,R)
             ,set_configuration_option(resolutions,S)
             ,set_configuration_option(recursive_reduction,RR)
@@ -156,6 +224,14 @@ setup(D):-
 %!      write_dataset(+Dataset) is det.
 %
 %       Write the metagol dataset for the given er Dataset.
+%
+%       Dataset is the name of a dataset, in particular, the file name
+%       (without extension) of the file defining the MIL problem for the
+%       dataset: one of [kin, robots].
+%
+%       Experiment datasets were originally created for Louise. This
+%       predicate initiates their translation to the representation of a
+%       MIL problem required by Metagol.
 %
 write_dataset(D):-
         memberchk(D-T,[kin-kin/2
@@ -183,12 +259,12 @@ run_kin:-
         ,once(setup(kin))
         ,T = kin/2
         ,M = acc
-        %,K = 100
-        %,float_interval(1,9,1,Ss)
+        ,K = 100
+        ,float_interval(1,9,1,Ss)
         % Uncomment the following two lines to test experiment setup.
         % Comment the two lines above, first!
-        ,K = 10
-        ,interval(1,10,1,Ss)
+        %,K = 5
+        %,interval(1,10,1,Ss)
         ,debug(progress,'~w: Starting on kin dataset',[L])
         ,learning_rate(T,M,K,Ss,_Ms,_SDs)
         ,debug(progress,'~w: Finished with kin dataset',[L]).
@@ -204,12 +280,12 @@ run_mtg_fragment:-
         ,once(setup(mtg_fragment))
         ,T = ability/2
         ,M = acc
-        %,K = 100
-        %,float_interval(1,9,1,Ss)
+        ,K = 100
+        ,float_interval(1,9,1,Ss)
         % Uncomment the following two lines to test experiment setup.
         % Comment the two lines above, first!
-        ,K = 10
-        ,interval(1,10,1,Ss)
+        %,K = 5
+        %,interval(1,10,1,Ss)
         ,debug(progress,'~w: Starting on mtg_fragment dataset',[L])
         ,learning_rate(T,M,K,Ss,_Ms,_SDs)
         ,debug(progress,'~w: Finished with mtg_fragment dataset',[L]).
@@ -220,18 +296,20 @@ run_mtg_fragment:-
 %
 %       Run a learning rate experiment on the robots.pl dataset.
 %
+%       Note that the MIL problem in robots.pl is described as "grid
+%       world navigation" in the paper.
+%
 run_robots:-
         configuration:learner(L)
         ,once(setup(robots))
         ,T = move/2
         ,M = acc
-        %,K = 10
-        %,float_interval(1,9,1,Ss)
+        ,K = 10
+        ,float_interval(1,9,1,Ss)
         % Uncomment the following two lines to test experiment setup.
         % Comment the two lines above, first!
-        ,K = 10
-        ,interval(1,10,1,Ss)
-        ,move_generator:write_dataset
+        %,K = 5
+        %,interval(1,10,1,Ss)
         ,debug(progress,'~w: Starting on robots dataset',[L])
         ,learning_rate(T,M,K,Ss,_Ms,_SDs)
         ,debug(progress,'~w: Finished with robots dataset',[L]).
@@ -245,6 +323,8 @@ run_robots:-
 %       in the closed interval [I,K] increasing by stride J, then
 %       dividing each by 10.
 %
+%       Used to generate a list of sampling sizes for experiments.
+%
 float_interval(I,K,J,Ss):-
         interval(I,K,J,Is)
         ,findall(S,(member(I_,Is)
@@ -256,9 +336,10 @@ float_interval(I,K,J,Ss):-
 %
 %       Set the Value of the Name'd option in a configuration Module.
 %
-%       Counterpart to set_configuration_option/2, to dynamically set
-%       configuration options for the learning rate experiment script
-%       and datasets used in learning rate experiments.
+%       Counterpart to set_configuration_option/2 defined in Louise,
+%       used to dynamically set configuration options for the learning
+%       rate experiment script and datasets used in learning rate
+%       experiments.
 %
 %       Use this to set configuration options defined in
 %       learning_rate_configuration.pl and datasets, particularly
