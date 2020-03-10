8e71e30a9798b61ac92829048de42b2e1a7f44a6
YeGoblynQueenne@splinter <e.patsantzis17@imperial.ac.uk>
2020-03-10 00:34:48 +0000

    Updated bash script with metagol data file writing calls.

--- experiments/learning_rate/scripts/run_learning_rate.sh
+++ experiments/learning_rate/scripts/run_learning_rate.sh
@@ -14,6 +14,10 @@ cd $to_script_from_learners
 
 cd $metagol_root
 
+swipl -s $script -g 'write_dataset(kin)' -t halt
+swipl -s $script -g 'write_dataset(mtg_fragment)' -t halt
+swipl -s $script -g 'write_dataset(robots)' -t halt
+
 swipl -s $script -g run_kin -t halt &
 swipl -s $script -g run_mtg_fragment -t halt &
 swipl -s $script -g run_robots -t halt &
